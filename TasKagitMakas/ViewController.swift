7015a21e498411e1043605b5b45a52ee5e603341
GÜNÇAĞ YILDIZ <guncagyildiz@gmail.com>
2018-11-11 16:17:46 +0300

    Initial Commit

---
+++ TasKagitMakas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TasKagitMakas
+//
+//  Created by GÜNÇAĞ YILDIZ on 11.11.2018.
+//  Copyright © 2018 Raks Yazılım. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TasKagitMakasTests/TasKagitMakasTests.swift
@@ -0,0 +1,34 @@
+//
+//  TasKagitMakasTests.swift
+//  TasKagitMakasTests
+//
+//  Created by GÜNÇAĞ YILDIZ on 11.11.2018.
+//  Copyright © 2018 Raks Yazılım. All rights reserved.
+//
+
+import XCTest
+@testable import TasKagitMakas
+
+class TasKagitMakasTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
